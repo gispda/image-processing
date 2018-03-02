dcb9f63c03dd76b6753d6cf19029a08c085ea1cb
catherine lee <clee200@ucsc.edu>
2018-03-01 19:03:10 -0800

    added bitwise and, updated main w function

--- bitwiseand.py
+++ bitwiseand.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-CANNY =10
+CANNY =1000
 
 cap=cv2.VideoCapture(0)
 lower_red = np.array([0,100,100])
@@ -18,7 +18,8 @@ while True:
 
 
     mask=cv2.inRange(hsv,lowerBlue,upperBlue)
-    
+    mask = cv2.bilateralFilter(mask,1,10,120)
+
     res=cv2.bitwise_and(frame,frame, mask=mask)
     
     edges  = cv2.Canny( res , 10, CANNY )
@@ -31,17 +32,24 @@ while True:
             
             #c = max(cont, key=cv2.contourArea) #find the max contour 
 
-            if(len(approx) ==3):
+            if(len(approx) ==4):
                 #SQUARES NOT WORKING BUT TRI IS PERFECT  
-                print("issa triangle")
+                print("sq")
                 (x,y,w,h) = cv2.boundingRect(approx)
                 cv2.drawContours(frame, [approx], -1, (255,0,0), 2)
-                cv2.putText(frame, 'blue triangle', (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0))
-
+                cv2.putText(frame, 'blue sq', (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0))
+#            elif(len(approx) ==3):
+#                print("tri")
+#                (x,y,w,h) = cv2.boundingRect(approx)
+#                cv2.drawContours(frame, [approx], -1, (255,0,0), 2)
+#                cv2.putText(frame, 'sblue tri', (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0))
+            else:
+                pass
     
     cv2.imshow('frame',frame)
     cv2.imshow('mask', mask)
     cv2.imshow('res', res)
+    cv2.imshow('edges',edges)
 
     k=cv2.waitKey(5)& 0xFF
     if k==27:
--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import numpy as np
 #import imutils 
 
 
-CANNY = 250
 
 #for seeing if there are squares
 isSquares = False 
+
+CANNY = 250
 MORPH = 7
 
 
 cap = cv2.VideoCapture(0)
+
+#blue
+BLUE = (0, 0, 255)
+RED =  (255,0,0)
 
 
 # Blue range \]
@@ -46,7 +51,31 @@ corners = np.array(
 
 pts_dst = np.array( corners, np.float32 )
 
+def findShapes(colorMask,COLOR, shapeLen, colorString,shapeString ):
+    #color is the thing you want to find aka the mast
+    #shape 
+    #just find both shapes ? 
+    colorMask = cv2.bilateralFilter(colorMask, 1,10,120)
+    edges  = cv2.Canny( colorMask , 10, CANNY)
+    _, contours, hierarchy = cv2.findContours( edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
+    
+    for cont in contours:
+        area = cv2.contourArea(cont)
+        if area > 300:
+            arc_len = cv2.arcLength(cont,True)
+            approx = cv2.approxPolyDP(cont, 0.1*arc_len, True)
+            
+            if(len(approx) ==shapeLen): #REMEMBER OR 
+                print("found" + colorString + " " + shapeString)
+                (x,y,w,h) = cv2.boundingRect(approx)
+                cv2.drawContours( frame, [approx], -1, COLOR, 2 )
+                cv2.putText(frame, colorString + " " + shapeString,(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
+            else:
+                pass 
+        
+    print("in function")
+    
 while True:
     # Take each frame
     _, frame = cap.read()
@@ -59,6 +88,8 @@ while True:
     maskRed = cv2.inRange(hsv, lowerRed, upperRed)
     maskYellow = cv2.inRange(hsv, lowerYellow, upperYellow)
     
+    #gotta make a black mask for finding Numbers
+    
     colorArr = [maskBlue, maskRed, maskYellow]
 
     maskBlueBilateral = cv2.bilateralFilter(maskBlue,1,10,120)
@@ -67,7 +98,6 @@ while True:
     closed = cv2.morphologyEx( edgesBlue, cv2.MORPH_CLOSE, kernelShape ) #fill in noisy spots
     _, blueContours, hierarchy = cv2.findContours( closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
 
-   
 
    
     #get contours for square objects
@@ -80,18 +110,23 @@ while True:
             #c = max(cont, key=cv2.contourArea) #find the max contour 
 
             if(len(approx) ==4):
-                (x,y,w,h) = cv2.boundingRect(approx)
                 print("square looking headass ")
+                #SQUARES NOT WORKING BUT TRI IS PERFECT  
+
+                (a,b,c,d) = cv2.boundingRect(approx)
                 isSquares = True
                 pts_src = np.array( approx, np.float32 )
                 h, status = cv2.findHomography( pts_src, pts_dst )
                 out = cv2.warpPerspective( maskBlueBilateral, h, ( int( _width + _margin * 2 ), int( _height + _margin * 2 ) ) )
                 cv2.drawContours( frame, [approx], -1, ( 255, 0, 0 ), 2 )
-                cv2.putText(frame, 'blue square',(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
+                cv2.putText(frame, 'blue square',(a,b),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
 
             else:
-                pass 
+                pass
+    findShapes(maskRed,RED, 3,"red ","Triangle")
+    #findShapes(maskBlue, "blue", 4, "square")
+    
 
         
     #Make frame tracking object
@@ -101,43 +136,14 @@ while True:
     cv2.imshow('mask',maskBlue)
 
     cv2.imshow('Blue Object Edges', edgesBlue)
-    if isSquares:
-        cv2.imshow('output Squares', out)
+#    if isSquares:
+#        cv2.imshow('output Squares', out)
         
     # Write frame to file
     #frame = cv2.flip(frame,0)
     if cv2.waitKey(5) == 27:
         break
-def findShapes(colorMask,shapeArray,colorString):
-    #color is the thing you want to find aka the mast
-    #shape 
-    #just find both shapes ? 
-    edges  = cv2.Canny( colorMask , 10, CANNY )
-    kernelShape = cv2.getStructuringElement( cv2.MORPH_RECT, ( MORPH, MORPH ) )
-    closed = cv2.morphologyEx( edges, cv2.MORPH_CLOSE, kernelShape ) #fill in noisy spots
-    _, contours, hierarchy = cv2.findContours( closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
-    
-    for cont in contours:
-        area = cv2.contourArea(cont)
-        if area > 300:
-            arc_len = cv2.arcLength(cont,True)
-            approx = cv2.approxPolyDP(cont, 0.1*arc_len, True)
-            
-            if(len(approx) ==4): #REMEMBER OR 
-                (x,y,w,h) = cv2.boundingRect(approx)
-                print("square")
-                pts_src = np.array( approx, np.float32 )
-                h, status = cv2.findHomography( pts_src, pts_dst )
-                #out = cv2.warpPerspective( colorMask, h, ( int( _width + _margin * 2 ), int( _height + _margin * 2 ) ) )
-                cv2.drawContours( frame, [approx], -1, ( 255, 0, 0 ), 2 )
-                cv2.putText(frame, colorString +" square",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
-            elif(len(approx) == 3):
-                print("found this triangle")
-            else:
-                pass 
-        
-    print("in function")
     
 cap.release()
 cv2.destroyAllWindows()
